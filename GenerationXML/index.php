42fbffb89c04c81443baeebff70cd7fd4ada79c5
Estevan <estevan.gay@gmail.com>
2019-09-06 12:39:04 -0400

    Exemple de script php pour la generation de fichier xml et son resultat

---
+++ GenerationXML/index.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once("BaseDeDonnees.php");
+
+$connection = new BaseDeDonnees();
+//Connection à la BDD
+$connection->connect();
+
+//Récupération du lien
+$c = $connection->getLien();
+
+//Requete et stockage
+$result = pg_query($c,"SELECT * FROM account");
+$row = pg_fetch_row($result);
+
+// TESTS
+// echo $row[0];
+// echo '<pre>';
+// print_r($row);
+// echo '</pre>';
+
+
+//Creation du DOMDocument
+$doc = new DOMDocument();
+
+$racine = $doc->createElement('G');
+$noeudL = $doc->createElement('L');
+$noeudP = $doc->createElement('P');
+
+$elementAttribute1 = $doc->createAttribute('x');
+$elementAttribute1->value = $row[1];
+$elementAttribute2 = $doc->createAttribute('y');
+$elementAttribute2->value = $row[2];
+
+$noeudP->appendChild($elementAttribute1);
+$noeudP->appendChild($elementAttribute2);
+
+$noeudL->appendChild($noeudP);
+$racine->appendChild($noeudL);
+
+$doc->appendChild($racine);
+
+
+$doc->save('test4.xml');
+
+$connection->disconnect();
+
+?>
