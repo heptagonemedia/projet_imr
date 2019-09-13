a9d60d5e9744b6b21a7388bfecb6ba6885670801
BigLazyBrother <tcousson@wanadoo.fr>
2019-09-13 14:49:47 -0400

    relecture du code

--- pgAgent/validation_coordonnees.sql
+++ pgAgent/validation_coordonnees.sql
@@ -1,4 +1,8 @@
-CREATE OR REPLACE FUNCTION distance(lat1 FLOAT, lon1 FLOAT, lat2 FLOAT, lon2 FLOAT) RETURNS FLOAT AS $$
+CREATE OR REPLACE FUNCTION distance(lat1 FLOAT, lon1 FLOAT, lat2 FLOAT, lon2 FLOAT)
+
+RETURNS FLOAT
+AS $$
+
 DECLARE                                                   
     x float = 111.12 * (lat2 - lat1);                           
     y float = 111.12 * (lon2 - lon1) * cos(lat1 / 92.215);        
@@ -8,9 +12,28 @@ END
 $$ LANGUAGE plpgsql;
 
 
-INSERT INTO donnee_valides(id_bouee, temperature, salinite, debit,  date_temps, longitude, latitude,batterie) 
-SELECT donnee_bouees.id_bouee,donnee_bouees.temperature,donnee_bouees.salinite,donnee_bouees.debit,donnee_bouees.date_temps,donnee_bouees.longitude,donnee_bouees.latitude,donnee_bouees.batterie
+INSERT INTO donnee_valides(
+	id_bouee
+	, temperature
+	, salinite
+	, debit
+	, date_temps
+	, longitude
+	, latitude
+	, batterie) 
+SELECT donnee_bouees.id_bouee
+	, donnee_bouees.temperature
+	, donnee_bouees.salinite
+	, donnee_bouees.debit
+	, donnee_bouees.date_temps
+	, donnee_bouees.longitude
+	, donnee_bouees.latitude
+	, donnee_bouees.batterie
 FROM donnee_bouees
 LEFT JOIN bouees
 ON bouees.id = donnee_bouees.id_bouee
-WHERE distance(bouees.latitude,bouees.longitude,donnee_bouees.latitude,donnee_bouees.longitude) < 0.010;
+WHERE distance(
+	bouees.latitude
+	, bouees.longitude
+	, donnee_bouees.latitude
+	, donnee_bouees.longitude) < 0.010;
