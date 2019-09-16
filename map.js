ddcb151cd8f56c0e884806903b457b12a1b70a8f
Louis Pascuzzi <pascuzzilouis@gmail.com>
2019-09-16 18:16:06 -0400

    rajout d une ligne de la fonction initMap supprimée par erreur

--- map.js
+++ map.js
@@ -68,43 +68,6 @@ function autorisationReduireSidebar() {
 var fermeture = true;
 var intervalle = setInterval(autorisationReduireSidebar, 1000);
 
-<<<<<<< HEAD
-=======
- // On initialise la latitude et la longitude de Paris (centre de la carte)
- var lat = 48.852969;
- var lon = -67.533555;
- var macarte = null;
- // Fonction d'initialisation de la carte
- function initMap() {
-     // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-     macarte = L.map('map', {zoomControl : false, attributionControl : false}).setView([49.210186, -67.433494], 8);
-     // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
-     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-         // Il est toujours bien de laisser le lien vers la source des données
-         attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-         minZoom: 1,
-         maxZoom: 20
-     }).addTo(macarte);
-     var url = document.location.href;
-    if(url.indexOf('bouee')==-1){
-      var listeCoords = new Array();
-      listeCoords.push(49.0523948);listeCoords.push(-68.283337);
-      for(var i=2;i<15;i+=2){
-        listeCoords.push(listeCoords[i-2]+0.1);
-        listeCoords.push(listeCoords[i-1 ]+0.33);
-      }
-      for(var i=0;i<29;i+=2){
-        L.marker([listeCoords[i], listeCoords[i+1]]).addTo(macarte);
-        console.log(listeCoords[i]+','+listeCoords[i+1])
-        
-      }
-
-      
-    }else{
-        macarte.setView([49.0523948, -68.283337], 10);
-        L.marker([49.0523948, -68.283337]).addTo(macarte);
-    }
->>>>>>> 7b18c80b6cbf92d5b35ce6c240f2ab47e3e4f87a
 
 
 function getBrowserSize(){
--- script.js
+++ script.js
@@ -350,8 +350,7 @@ var macarte = null;
 // Fonction d'initialisation de la carte
 function initMap() {
     // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-    macarte = L.map('map').setView([49.210186, -67.433494], 8);
-    // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+    macarte = L.map('map', {zoomControl : false, attributionControl : false}).setView([49.210186, -67.433494], 8);    // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         // Il est toujours bien de laisser le lien vers la source des données
         attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
