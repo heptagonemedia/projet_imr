df08f5a23a4d8c488af0f8e0f0505a7c5eac927b
BigLazyBrother <tcousson@wanadoo.fr>
2019-09-19 16:28:58 -0400

    Adaptation des fonctions pour le nouveau MCD, Écriture de la fonction de vérif des données. Ne pas executer : fonction non terminée.

--- pgAgent/TEST.sql
+++ pgAgent/TEST.sql
@@ -1,3 +1,6 @@
+--------------------------------------------------------------------------------
+-- Suppression de tous vestige de l'ancien modèle
+
 DROP TABLE IF EXISTS public.recherche_enregistrees;
 DROP TABLE IF EXISTS public.donnee_valides;
 DROP TABLE IF EXISTS public.donnee_invalides;
@@ -6,180 +9,256 @@ DROP TABLE IF EXISTS public.bouees;
 DROP FUNCTION IF EXISTS distance;
 DROP FUNCTION IF EXISTS intervale_incertiture;
 
-CREATE TABLE public.bouees (
-    id            serial        PRIMARY KEY
-    , numero      integer       NOT NULL
-    , description text COLLATE pg_catalog."default" NOT NULL
-    , date_debut  timestamp without time zone NOT NULL
-    , longitude   numeric(10,8) NOT NULL
-    , latitude    numeric(10,8) NOT NULL
-    )
-    WITH (
-        OIDS = FALSE
-    )
-    TABLESPACE pg_default;
-
-ALTER TABLE public.bouees
-    OWNER to postgres;
-
-CREATE TABLE public.donnee_bouees (
-    id            bigserial     PRIMARY KEY
-    , id_bouee    integer       NOT NULL
-    , temperature numeric(4,2)  NOT NULL
-    , salinite    numeric(4,2)  NOT NULL
-    , debit       numeric(5,2)  NOT NULL
-    , date_temps  timestamp without time zone NOT NULL
-    , longitude   numeric(10,8) NOT NULL
-    , latitude    numeric(10,8) NOT NULL
-    , batterie    integer       NOT NULL
-    , CONSTRAINT donnee_bouees_id_bouee_fkey FOREIGN KEY (id_bouee)
-        REFERENCES public.bouees (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-    )
-    WITH (
-        OIDS = FALSE
-    )
-    TABLESPACE pg_default;
-
-ALTER TABLE public.donnee_bouees
-    OWNER to postgres;
-
-CREATE TABLE public.donnee_invalides (
-    id            bigserial     PRIMARY KEY
-    , id_bouee    integer       NOT NULL
-    , temperature numeric(4,2)  NOT NULL
-    , salinite    numeric(4,2)  NOT NULL
-    , debit       numeric(5,2)  NOT NULL
-    , date_temps  timestamp without time zone NOT NULL
-    , longitude   numeric(10,8) NOT NULL
-    , latitude    numeric(10,8) NOT NULL
-    , batterie    integer       NOT NULL
-    , CONSTRAINT donnee_bouees_id_bouee_fkey FOREIGN KEY (id_bouee)
-        REFERENCES public.bouees (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-    )
-    WITH (
-        OIDS = FALSE
-    )
-    TABLESPACE pg_default;
-
-ALTER TABLE public.donnee_invalides
-    OWNER to postgres;
-
-CREATE TABLE public.donnee_valides (
-    id            bigserial     PRIMARY KEY
-    , id_bouee    integer       NOT NULL
-    , temperature numeric(4,2)  NOT NULL
-    , salinite    numeric(4,2)  NOT NULL
-    , debit       numeric(5,2)  NOT NULL
-    , date_temps  timestamp without time zone NOT NULL
-    , longitude   numeric(10,8) NOT NULL
-    , latitude    numeric(10,8) NOT NULL
-    , batterie    integer       NOT NULL
-    , CONSTRAINT donnee_bouees_id_bouee_fkey FOREIGN KEY (id_bouee)
-        REFERENCES public.bouees (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-    )
-    WITH (
-        OIDS = FALSE
-    )
-    TABLESPACE pg_default;
-
-ALTER TABLE public.donnee_valides
-    OWNER to postgres;
-
-CREATE TABLE IF NOT EXISTS public.recherche_enregistrees (
-    id                  serial  NOT NULL
-    , id_bouee          integer NOT NULL
-    , frequence_annee   integer NOT NULL
-    , frequence_mois    integer NOT NULL
-    , frequence_jour    integer NOT NULL
-    , frequence_heure   integer NOT NULL
-    , frequence_minute  integer NOT NULL
-    , frequence_seconde integer NOT NULL
-    , CONSTRAINT recherche_enregistrees_pkey PRIMARY KEY (id)
-    , CONSTRAINT recherche_enregistrees_id_bouee_fkey FOREIGN KEY (id_bouee)
-        REFERENCES public.bouees (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-    )
-    WITH (
-        OIDS = FALSE
-    )
-    TABLESPACE pg_default;
-
-ALTER TABLE public.recherche_enregistrees
-    OWNER to "userLambda";
-
 --------------------------------------------------------------------------------
+-- Mise en place du nouveau modèle
 
-CREATE OR REPLACE FUNCTION distance(lat1 FLOAT, lon1 FLOAT, lat2 FLOAT, lon2 FLOAT)
+DROP TABLE IF EXISTS calcul_enregistre;
+DROP TABLE IF EXISTS type_calcul;
+DROP TABLE IF EXISTS mesure;
+DROP TABLE IF EXISTS type_donnee_mesuree;
+DROP TABLE IF EXISTS donnee_traitee;
+DROP TABLE IF EXISTS historique_donnee_bouee;
+DROP TABLE IF EXISTS bouee;
+DROP TABLE IF EXISTS region;
 
-    RETURNS FLOAT
-AS $$
+CREATE TABLE region(
+                       id_region serial PRIMARY KEY,
+                       etiquette text
+);
 
-DECLARE
-    x float = 111.12 * (lat2 - lat1);
-    y float = 111.12 * (lon2 - lon1) * cos(lat1 / 92.215);
-BEGIN
-    RETURN sqrt(x * x + y * y);
-END
-$$ LANGUAGE plpgsql;
-;
+CREATE TABLE bouee(
+                      id_bouee serial PRIMARY KEY,
+                      etiquette text,
+                      longitude_reference float NOT NULL,
+                      latitude_reference float NOT NULL,
+                      id_region integer NOT NULL,
+                      CONSTRAINT region_bouee_fk
+                          FOREIGN KEY (id_region)
+                              REFERENCES region(id_region)
+                              ON DELETE CASCADE
+                              ON UPDATE CASCADE
+);
+
+CREATE TABLE historique_donnee_bouee(
+                                        id_historique_donnee_bouee bigserial PRIMARY KEY,
+                                        id_bouee integer NOT NULL,
+                                        longitude_reelle float NOT NULL,
+                                        latitude_reelle float NOT NULL,
+                                        date_saisie timestamp without time zone NOT NULL,
+                                        CONSTRAINT bouee_historique_donnee_bouee_fk
+                                            FOREIGN KEY (id_bouee)
+                                                REFERENCES bouee(id_bouee)
+                                                ON DELETE CASCADE
+                                                ON UPDATE CASCADE
+);
+
+CREATE TABLE donnee_traitee(
+                               id_donnee_traitee serial PRIMARY KEY,
+                               id_historique_donnee_bouee bigint NOT NULL,
+                               valide boolean NOT NULL,
+                               CONSTRAINT historique_donnee_bouee_donnee_traitee_fk
+                                   FOREIGN KEY (id_historique_donnee_bouee)
+                                       REFERENCES historique_donnee_bouee(id_historique_donnee_bouee)
+                                       ON DELETE CASCADE
+                                       ON UPDATE CASCADE
+);
+
+CREATE TABLE type_donnee_mesuree(
+                                    id_type_donnee_mesuree serial PRIMARY KEY,
+                                    etiquette text NOT NULL
+);
+
+CREATE TABLE mesure(
+                       id_mesure serial PRIMARY KEY,
+                       id_historique_donnee_bouee bigint NOT NULL,
+                       id_type_donnee_mesuree integer NOT NULL,
+                       valeur float NOT NULL,
+                       CONSTRAINT historique_donnee_bouee_mesure_fk
+                           FOREIGN KEY (id_historique_donnee_bouee)
+                               REFERENCES historique_donnee_bouee(id_historique_donnee_bouee)
+                               ON DELETE CASCADE
+                               ON UPDATE CASCADE,
+                       CONSTRAINT type_donnee_mesuree_mesure_fk
+                           FOREIGN KEY (id_type_donnee_mesuree)
+                               REFERENCES type_donnee_mesuree(id_type_donnee_mesuree)
+                               ON DELETE CASCADE
+                               ON UPDATE CASCADE
+);
+
+CREATE TABLE type_calcul(
+                            id_type_calcul serial PRIMARY KEY,
+                            etiquette text
+);
+
+CREATE TABLE calcul_enregistre(
+                                  id_calcul_enregistre serial PRIMARY KEY,
+                                  date_debut timestamp without time zone NOT NULL,
+                                  date_fin timestamp without time zone NOT NULL,
+                                  frequence float NOT NULL,
+                                  valeur float NOT NULL,
+                                  id_bouee integer NOT NULL,
+                                  id_type_donnee_mesuree integer NOT NULL,
+                                  id_type_calcul integer NOT NULL,
+                                  prevu boolean NOT NULL,
+                                  CONSTRAINT donnee_mesuree_calcul_enregistre_fk
+                                      FOREIGN KEY (id_type_donnee_mesuree)
+                                          REFERENCES type_donnee_mesuree(id_type_donnee_mesuree)
+                                          ON DELETE CASCADE
+                                          ON UPDATE CASCADE,
+                                  CONSTRAINT type_calcul_enregistre_fk
+                                      FOREIGN KEY (id_type_calcul)
+                                          REFERENCES type_calcul(id_type_calcul)
+                                          ON DELETE CASCADE
+                                          ON UPDATE CASCADE,
+                                  CONSTRAINT bouee_calcul_enregistrer_fk
+                                      FOREIGN KEY (id_bouee)
+                                          REFERENCES bouee(id_bouee)
+                                          ON DELETE CASCADE
+                                          ON UPDATE CASCADE
+);
 
 
 --------------------------------------------------------------------------------
+-- Nouvelle version de la fonction de vérification des données.
 
-CREATE OR REPLACE FUNCTION intervale_incertiture(val NUMERIC(5,2), colonne TEXT, temps TIMESTAMP, id_bouee_liee INTEGER)
+CREATE OR REPLACE FUNCTION nouveau_intervale_incertitude(id_historique INT, type_donnee TEXT)
 
     RETURNS BOOLEAN
     LANGUAGE 'plpgsql'
-AS $$
+
+AS $BODY$
 
 DECLARE
+    id_bouee_a_check INT;
+    valeur_mesure NUMERIC(5,2); -- valeur à vérifier
+    temps_present TIMESTAMP; -- valeur de x dans l'équation.
+    -- On ne peut pas prendre l'id → 75 000 entrées par secondes.
+    -- dans l'équation, y(x) désigne "SELECT mesure.qqch WHERE temps = x";
 
-valeurLue NUMERIC(5,2);
-moyenne NUMERIC(5,2);
-cycles INT;
+    profondeur_verification INT; -- valeur de n dans l'équation
+    moyenne NUMERIC(5,2);
+    ecart_type NUMERIC(5,2);
+
+    -- soit l'équation :
+    -- moyenne = ( SIGMA de i = 1 à n, de y(x-i) ) le tout divisé par n-1
+
+    resultat BOOLEAN; -- pour débugger uniquement.
 
 BEGIN
 
-    cycles := 25; -- On consulte ces dernières valeurs pour établir la moyenne de comparaison
+    valeur_mesure := (
+        SELECT m.valeur
+        FROM historique_donnee_bouee AS hdb
+        JOIN mesure AS m ON m.id_historique_donnee_bouee = hdb.id_historique_donnee_bouee
+        JOIN type_donnee_mesuree tdm ON m.id_type_donnee_mesuree = tdm.id_type_donnee_mesuree
+        WHERE hdb.id_historique_donnee_bouee = id_historique
+        AND tdm.etiquette = type_donnee
+    ); -- valeur à tester
 
-    FOR i IN 1..cycles LOOP
-            valeurLue := (SELECT colonne FROM donnee_bouees WHERE id_bouee = id_bouee_liee AND date_temps = temps - (i * INTERVAL '1 SECOND'));
-            moyenne := moyenne + valeurLue;
+    id_bouee_a_check := (
+        SELECT hdb.id_bouee
+        FROM historique_donnee_bouee AS hdb
+        WHERE hdb.id_historique_donnee_bouee = id_historique
+    ); -- id de la bouée à check
+    temps_present := (
+        SELECT hdb.date_saisie
+        FROM historique_donnee_bouee AS hdb
+        WHERE hdb.id_historique_donnee_bouee = id_historique
+    ); -- temps à x
+
+    profondeur_verification := 3; -- On consulte ces valeurs pour établir la moyenne de l'intervale d'incertitude.
+
+    -- Calcul de la moyenne
+    FOR i IN 1..profondeur_verification LOOP
+        moyenne := moyenne + (
+            SELECT m.valeur
+            FROM historique_donnee_bouee AS hdb
+            JOIN mesure m ON hdb.id_historique_donnee_bouee = m.id_historique_donnee_bouee
+            JOIN type_donnee_mesuree tdm ON m.id_type_donnee_mesuree = tdm.id_type_donnee_mesuree
+            WHERE hdb.id_bouee = id_bouee_a_check
+            AND hdb.date_saisie = temps_present - (i * INTERVAL '1 SECOND')
+            AND tdm.etiquette = type_donnee
+        ); -- val actu - x
     END LOOP;
-    moyenne := moyenne/(cycles-1);
 
-    RETURN (NOT (val*0.2 < moyenne OR val*1.8 > moyenne)); -- Si la valeur est trop petite ou trop grande : FALSE.
+    moyenne := moyenne/(profondeur_verification-1);
 
-END
-$$ 
-;
+    -- Calcul de l'écart type
+    FOR i IN 1..profondeur_verification LOOP
+        ecart_type := POWER(ecart_type +(
+            SELECT m.valeur
+            FROM historique_donnee_bouee AS hdb
+            JOIN mesure m ON hdb.id_historique_donnee_bouee = m.id_historique_donnee_bouee
+            JOIN type_donnee_mesuree tdm ON m.id_type_donnee_mesuree = tdm.id_type_donnee_mesuree
+            WHERE hdb.id_bouee = id_bouee_a_check
+            AND hdb.date_saisie = temps_present - (i * INTERVAL '1 SECOND')
+            AND tdm.etiquette = type_donnee
+        ), 2) - POWER(moyenne, 2);
+
+        -- REMPLACER LE SELECT PAR UN ARRAY (on utilise le même SELECT pour la moyenne et l'écart-type)
+
+    END LOOP;
+
+    ecart_type := SQRT((1/profondeur_verification) * ecart_type);
+
+    -- Calcul de l'intervale de confiance.
+
+    --
+    -- WORK IN PROGRESS AREA
+    --
+
+    -- RETURN (NOT (valeur_mesure*0.2 < moyenne OR valeur_mesure*1.8 > moyenne));
+    -- Si la valeur est trop petite ou trop grande : FALSE.
+
+    -- PLPGSQL ne gère pas un return direct, je suis obligé de passer par des IF ELSE
+    -- C'est pas la vrai formule de l'intervale de confiance, mais c'est assez précis
+    -- et le code est déjà très lourd. On a une marge de 5%.
+    IF (valeur_mesure*0.05 < moyenne OR valeur_mesure*1.05 > moyenne) THEN
+        resultat := FALSE;
+    ELSE
+        resultat := TRUE;
+    END IF;
+
+    RETURN resultat;
+
+END;
+
+
+
+
+$BODY$;
 
 --------------------------------------------------------------------------------
 -- données de test de la fonction
 
-INSERT INTO bouees (numero, description, date_debut, longitude, latitude)
-VALUES (1, 'bouée 1', '2019-09-13 21:00:00', 12, 12);
+INSERT INTO region (etiquette)
+VALUES ('Region1');
 
-INSERT INTO donnee_bouees (id_bouee, temperature, salinite, debit, date_temps, longitude, latitude, batterie)
-VALUES (1, 12.3, 12, 12, '2019-09-13 21:06:00', 12, 12, 12);
+INSERT INTO bouee (etiquette, longitude_reference, latitude_reference, id_region)
+VALUES ('bouée 1', 12, 12, 1);
 
-INSERT INTO donnee_bouees (id_bouee, temperature, salinite, debit, date_temps, longitude, latitude, batterie)
-VALUES (1, 12.4, 12, 12, '2019-09-13 21:06:01', 12, 12, 12);
+INSERT INTO type_donnee_mesuree (etiquette)
+VALUES ('salinite'); -- type 1 = salinite
 
-INSERT INTO donnee_bouees (id_bouee, temperature, salinite, debit, date_temps, longitude, latitude, batterie)
-VALUES (1, 12.5, 12, 12, '2019-09-13 21:06:03', 12, 12, 12);
+INSERT INTO historique_donnee_bouee (id_bouee, longitude_reelle, latitude_reelle, date_saisie)
+VALUES (1, 12, 12, '2019-09-19 15:00:00'); -- historique 1
 
-INSERT INTO donnee_bouees (id_bouee, temperature, salinite, debit, date_temps, longitude, latitude, batterie)
-VALUES (1, 12.4, 12, 12, '2019-09-13 21:06:04', 12, 12, 12);
+INSERT INTO mesure (id_historique_donnee_bouee, id_type_donnee_mesuree, valeur)
+VALUES (1, 1, 800000); -- historique 1 et type 1
 
-SELECT intervale_incertiture(12.4, 'temperature', '2019-09-13 21:06:04');
+INSERT INTO historique_donnee_bouee (id_bouee, longitude_reelle, latitude_reelle, date_saisie)
+VALUES (1, 12, 12, '2019-09-19 15:00:01'); -- histo 2
+
+INSERT INTO mesure (id_historique_donnee_bouee, id_type_donnee_mesuree, valeur)
+VALUES (2, 1, 25);
+
+INSERT INTO historique_donnee_bouee (id_bouee, longitude_reelle, latitude_reelle, date_saisie)
+VALUES (1, 12, 12, '2019-09-19 15:00:02'); -- histo 3
+
+INSERT INTO mesure (id_historique_donnee_bouee, id_type_donnee_mesuree, valeur)
+VALUES (3, 1, 25);
+
+SELECT nouveau_intervale_incertitude(3, 'salinite');
 
 
 
