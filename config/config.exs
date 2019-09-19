895ad06d9476049f06f6da0e0ef860ce74714d75
Adrian <adrian.paul.carrieres@gmail.com>
2019-09-19 16:24:43 -0400

    Création de la database, ajout des schémas

---
+++ config/config.exs
@@ -0,0 +1,10 @@
+use Mix.Config
+
+config :generateur_scenario, GenerateurScenario.Repo,
+  database: "generateur_scenario_repo",
+  username: "postgres",
+  password: "mission",
+  hostname: "localhost",
+  port: 5432
+
+  config :generateur_scenario, ecto_repos: [GenerateurScenario.Repo]
--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule GenerateurScenario.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {GenerateurScenario.Application, []}
     ]
   end
 
@@ -23,6 +24,8 @@ defmodule GenerateurScenario.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 end
