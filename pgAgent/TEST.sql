5945be633643db7b1d10b7a09f678216f8484623
BigLazyBrother <tcousson@wanadoo.fr>
2019-09-26 15:15:50 -0400

    Ajout de la fonction de validation de toutes les données de toutes les bouées dans un intervalle de temps donné.

--- pgAgent/TEST.sql
+++ pgAgent/TEST.sql
@@ -253,7 +253,49 @@ END;
 
 $BODY$;
 
+--------------------------------------------------------------------------------
+-- Fonction journalière de vérification des données.
 
+CREATE OR REPLACE FUNCTION verification_journaliere(heure_lancement TIMESTAMP)
+
+    RETURNS BOOLEAN -- il faudra voir ce qu'il faut return dans le cas où on se fout du return
+    LANGUAGE  'plpgsql'
+
+AS $BODY$
+
+DECLARE
+    heure_a_verifier TIMESTAMP; -- heure qu'on check, de heure_lancement (now) à heure_lancement - 24h.
+    -- intervale_temps pour pouvoir régler l'intervale à vérifer ? (changer 24h à 5minutes)
+    no_bouee_a_verifier INT; -- va de 1 inclus à mas_bouee inclus.
+    max_bouee INT; -- 75 000
+    id_historique INT; -- id de l'élément qu'on cherche par no_bouee_a_verifier et heure_a_verifier
+
+BEGIN
+    heure_a_verifier := heure_lancement;
+    max_bouee := 75000;
+
+    FOR no_bouee_a_verifier IN 1..max_bouee LOOP
+        WHILE heure_a_verifier < heure_lancement::timestamp + (INTERVAL '24 HOUR') LOOP --::timestamp
+            id_historique := (
+                SELECT id_historique_donnee_bouee
+                FROM historique_donnee_bouee
+                WHERE id_bouee = no_bouee_a_verifier
+                AND date_saisie = heure_a_verifier
+            );
+            IF verification_donnees_bouee(id_historique) <> FALSE THEN
+                INSERT INTO donnee_traitee (id_historique_donnee_bouee, valide)
+                VALUES (id_historique, TRUE);
+            ELSE
+                INSERT INTO donnee_traitee (id_historique_donnee_bouee, valide)
+                VALUES (id_historique, FALSE);
+            END IF;
+            heure_a_verifier := heure_a_verifier::timestamp + (INTERVAL '1 SECOND');
+        END LOOP; -- fin du While (cadre temporel par bouée)
+        heure_a_verifier := heure_lancement; -- On change de bouée, donc on reprend à 0s
+    END LOOP; -- fin du For (cadre du nombre des bouées)
+END;
+
+$BODY$;
 
 --------------------------------------------------------------------------------
 -- données de test de la fonction
