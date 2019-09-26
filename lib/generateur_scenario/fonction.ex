9c91b7a26fa00ffbb6bc1723332cc458f16389ed
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2019-09-25 22:16:12 -0400

    La plupart des fonctions sont normalement prêtes

--- lib/generateur_scenario/fonction.ex
+++ lib/generateur_scenario/fonction.ex
@@ -1,5 +1,5 @@
 defmodule GenerateurScenario.Fonction do
-    
+
 
     def calcul_amplitude(min, max) do
         (max - min)/2
@@ -13,7 +13,25 @@ defmodule GenerateurScenario.Fonction do
         max - calcul_amplitude(min, max)
     end
 
+    @spec fonction_sinusoidale_normale(number, number, number, number) :: float
     def fonction_sinusoidale_normale(instant, min, max, duree_cycle) do
         calcul_amplitude(min, max)*Math.sin(calcul_pulsation(duree_cycle)*instant) + calcul_decalage_en_x(min, max)
+    end
+
+    def fonction_sinusoidale_anormale(instant, duree_cycle, decalage) do
+        calcul_amplitude(decalage - 2, decalage + 2)*Math.sin(calcul_pulsation(duree_cycle)*instant) + decalage
+    end
+
+    @spec fonction_batterie_normale(number, number, number) :: number
+    def fonction_batterie_normale(instant, coefficient, ordonnee_origine) do
+        coefficient * instant + ordonnee_origine
+    end
+
+    def fonction_affine_anormale_croissante(instant) do
+        40*instant + 5
+    end
+
+    def fonction_affine_anormale_decroissante(instant) do
+        -40*instant + 5
     end
 end
