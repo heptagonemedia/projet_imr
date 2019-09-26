cf1cdfac99bddc8abd98cca97ab9e74b280c5ef2
Lucien Barcon <lucienbarcon@gmail.com>
2019-09-26 16:11:54 -0400

    Lecture xml par elixir des scenarios

--- lib/generateur_scenario/lireXml.ex
+++ lib/generateur_scenario/lireXml.ex
@@ -25,5 +25,34 @@ defmodule GenerateurScenario.LireXml do
         xmldoc |> xpath(~x"/config/region/debit/text()"l)
     end
 
+    def getScenarioBase() do
+        import SweetXml
+        {:ok, xmldoc} = File.read(Path.expand("./configuration.xml"))
+        xmldoc |> xpath(~x"/config/proportion/scenarioBase/text()"l)
+    end
+
+    def getNbScenario() do
+        import SweetXml
+        {:ok, xmldoc} = File.read(Path.expand("./configuration.xml"))
+        xmldoc |> xpath(~x"/config/proportion/nbScenario/text()"l)
+    end
+
+    def getScenarioNormaux() do
+        import SweetXml
+        {:ok, xmldoc} = File.read(Path.expand("./configuration.xml"))
+        xmldoc |> xpath(~x"/config/proportion/scenarioNormaux/text()"l)
+    end
+
+    def getScenarioHybride() do
+        import SweetXml
+        {:ok, xmldoc} = File.read(Path.expand("./configuration.xml"))
+        xmldoc |> xpath(~x"/config/proportion/scenarioHybride/text()"l)
+    end
+
+    def getScenarioRetarde() do
+        import SweetXml
+        {:ok, xmldoc} = File.read(Path.expand("./configuration.xml"))
+        xmldoc |> xpath(~x"/config/proportion/scenarioRetardes/text()"l)
+    end
 
 end
