338bbf4096269bf23a61a907beec7111998fa764
Estevan <estevan.gay@gmail.com>
2019-10-03 13:54:55 -0400

    Modification du header pour afficher sur la page d'accueil

--- header.php
+++ header.php
@@ -2,7 +2,9 @@
     <div class="nav-wrapper black">
         <div class="row">
 
-
+            <?php
+            // echo($_SERVER['REQUEST_URI']);
+            ?>
 
             <?php
             $titre = '';
@@ -39,8 +41,20 @@
 
                     <a href="accueil.php" class="breadcrumb">accueil</a>
                     <a href="formulaireRecherche.php" class="breadcrumb">formulaire</a>
-                    <a href="#" class="breadcrumb">Calcul : <?php if (isset($_POST['bouee'])){echo $_POST['bouee'];}else{echo " nom";} ?></a>
+                    <a href="#" class="breadcrumb">Calcul : <?php if (isset($_POST['bouee'])) {
+                                                                    echo $_POST['bouee'];
+                                                                } else {
+                                                                    echo " nom";
+                                                                } ?></a>
                 </div>
+            <?php
+            else :
+                $titre = 'Accueil';
+                ?>
+                <div class="col black m1 l4 center-align">
+                    <button href="#" data-target="slide-out" class="sidenav-trigger btn black white-text"><i class="material-icons" id="menu">menu</i></button>
+                </div>
+                <div class="col black m11 l4 center-align" style="font-size: 18pt"><?= $titre ?></div>
             <?php
             endif;
             ?>
