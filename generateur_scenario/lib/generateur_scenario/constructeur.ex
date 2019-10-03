8f3342869f026044f49edb83a31c9cf881b039cc
Adrian <adrian.paul.carrieres@gmail.com>
2019-10-03 16:16:48 -0400

    Bases des scénarios normaux posées

--- generateur_scenario/lib/generateur_scenario/constructeur.ex
+++ generateur_scenario/lib/generateur_scenario/constructeur.ex
@@ -5,20 +5,36 @@ defmodule GenerateurScenario.Constructeur do
     end
 
     def loop(parametres, n, type) when n <= 1 do
-        IO.puts "Hey"
+        creer_scenario(parametres, n, type)
     end
     
     def loop(parametres, n, type) do
-        IO.puts "Hey"
+        creer_scenario(parametres, n, type)
         loop(parametres, n - 1, type)
     end
 
 
     def preparer_scenario(id_scenario) do
-        %{temp: GenerateurScenario.Fonction.forger_parametres(Agents.Temperature.value),
+        %{temperature: GenerateurScenario.Fonction.forger_parametres(Agents.Temperature.value),
         salinite: GenerateurScenario.Fonction.forger_parametres(Agents.Salinite.value),
         debit: GenerateurScenario.Fonction.forger_parametres(Agents.Debit.value),
         batterie: 100,
         id_scenario: id_scenario}
+    end
+
+    def creer_scenario(parametres, n, type) do
+        scenario = %GenerateurScenario.Scenario{
+            id_scenario: parametres.id_scenario,
+            seconde: n,
+            temperature: GenerateurScenario.Fonction.fonction_sinusoidale(parametres.temperature, n),
+            salinite: GenerateurScenario.Fonction.fonction_sinusoidale(parametres.salinite, n),
+            debit: GenerateurScenario.Fonction.fonction_sinusoidale(parametres.debit, n),
+            longitude: 100.00,
+            latitude: 100.00,
+            batterie: parametres.batterie
+        }
+
+        changeset = GenerateurScenario.Scenario.changeset(scenario, %{})
+        GenerateurScenario.Repo.insert(changeset)
     end
 end
