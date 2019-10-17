daeb94d4ddcbb731250fe3e2f3a89f2ec6a8585a
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2019-10-17 15:59:22 -0400

    ajout des messages

--- recepteur/lib/recepteur_web/channels/room_channel.ex
+++ recepteur/lib/recepteur_web/channels/room_channel.ex
@@ -1,29 +1,15 @@
 defmodule RecepteurWeb.RoomChannel do
   use RecepteurWeb, :channel
 
-  def join("room:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+  def join("room:lobby", _message, socket) do
+    {:ok, socket}
+  end
+  def join("room:" <> _private_room_id, _params, _socket) do
+    {:error, %{reason: "unauthorized"}}
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (room:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast!(socket, "new_msg", %{body: body})
     {:noreply, socket}
-  end
-
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
   end
 end
