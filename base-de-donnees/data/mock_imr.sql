5bd16f5285cf72a48d155a8809a6d6919e34d317
BigLazyBrother <tcousson@wanadoo.fr>
2019-10-23 10:07:16 -0400

    Mise à jour des données de test selon la MÀJ du modèle

--- base-de-donnees/data/mock_imr.sql
+++ base-de-donnees/data/mock_imr.sql
@@ -1,7 +1,7 @@
-\COPY region (etiquette) FROM './mockdata/region.csv' DELIMITER ';' CSV;
+\COPY region (etiquette) FROM './mockdata/region.csv' DELIMITER ',' CSV;
 \COPY bouee (etiquette, longitude_reference, latitude_reference, id_region) FROM './mockdata/bouee.csv' DELIMITER ',' CSV;
-\COPY type_donnee_mesuree (etiquette) FROM './mockdata/type_donnee_mesuree.csv' DELIMITER ',' CSV;
-\COPY historique_donnee_bouee (id_bouee, longitude_reelle, latitude_reelle, date_saisie) FROM './mockdata/historique_donnee_bouee.csv' DELIMITER ',' CSV;
+\COPY type_donnee_mesuree (etiquette, unite) FROM './mockdata/type_donnee_mesuree.csv' DELIMITER ',' CSV;
+\COPY historique_donnee_bouee (id_bouee, longitude_reelle, latitude_reelle, date_saisie, batterie) FROM './mockdata/historique_donnee_bouee.csv' DELIMITER ',' CSV;
 \COPY mesure (id_historique_donnee_bouee, id_type_donnee_mesuree, valeur) FROM './mockdata/mesure.csv' DELIMITER ',' CSV;
 \COPY donnee_traitee (id_historique_donnee_bouee, valide) FROM './mockdata/donnee_traitee.csv' DELIMITER ',' CSV;
 \COPY type_calcul (etiquette) FROM './mockdata/type_calcul.csv' DELIMITER ',' CSV;
