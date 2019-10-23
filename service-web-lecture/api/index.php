5344db8231ff9b8b0fbd99200ce1840fd8c0c12b
loicbtd <loic.bert.marcel@gmail.com>
2019-10-23 10:40:22 -0400

    service-web-lecture: début implémentation classes DAO

---
+++ service-web-lecture/api/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('../secure/env.php');
+require_once('modele/Region.php');
+require_once('modele/Bouee.php');
+require_once('modele/HistoriqueDonneMesuree.php');
+require_once('modele/DonneeTraitee.php');
+require_once('modele/TypeDonneeMesuree.php');
+require_once('modele/Mesure.php');
+require_once('modele/TypeCalcul.php');
+require_once('modele/CalculEnregistre.php');
