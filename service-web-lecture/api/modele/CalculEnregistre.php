f4d1b4a19369cb284a30b5090d2903368e89b989
loicbtd <loic.bert.marcel@gmail.com>
2019-10-23 09:46:04 -0400

    service-web-lecture: création des classes modèle

--- service-web-lecture/api/modele/CalculEnregistre.php
+++ service-web-lecture/api/modele/CalculEnregistre.php
@@ -16,10 +16,219 @@ class CalculEnregistre
     const CLE_ETIQUETTE = "etiquette";
 
     private $idCalculEnregistre;
-    private $idBouee;
+    private $bouee;
     private $dateDebut;
     private $dateFin;
     private $frequence;
     private $valeur;
-    private $id
+    private $typeDonneeMesuree;
+    private $typeCalcul;
+    private $prevue;
+    private $cheminFichierXml;
+    private $etiquette;
+
+    /**
+     * CalculEnregistre constructor.
+     * @param $idCalculEnregistre
+     * @param $bouee
+     * @param $dateDebut
+     * @param $dateFin
+     * @param $frequence
+     * @param $valeur
+     * @param $typeDonneeMesuree
+     * @param $typeCalcul
+     * @param $prevue
+     * @param $cheminFichierXml
+     * @param $etiquette
+     */
+    public function __construct($idCalculEnregistre, $bouee, $dateDebut, $dateFin, $frequence, $valeur, $typeDonneeMesuree, $typeCalcul, $prevue, $cheminFichierXml, $etiquette)
+    {
+        $this->idCalculEnregistre = $idCalculEnregistre;
+        $this->bouee = $bouee;
+        $this->dateDebut = $dateDebut;
+        $this->dateFin = $dateFin;
+        $this->frequence = $frequence;
+        $this->valeur = $valeur;
+        $this->typeDonneeMesuree = $typeDonneeMesuree;
+        $this->typeCalcul = $typeCalcul;
+        $this->prevue = $prevue;
+        $this->cheminFichierXml = $cheminFichierXml;
+        $this->etiquette = $etiquette;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdCalculEnregistre()
+    {
+        return $this->idCalculEnregistre;
+    }
+
+    /**
+     * @param mixed $idCalculEnregistre
+     */
+    public function setIdCalculEnregistre($idCalculEnregistre)
+    {
+        $this->idCalculEnregistre = $idCalculEnregistre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBouee()
+    {
+        return $this->bouee;
+    }
+
+    /**
+     * @param mixed $bouee
+     */
+    public function setBouee($bouee)
+    {
+        $this->bouee = $bouee;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateDebut()
+    {
+        return $this->dateDebut;
+    }
+
+    /**
+     * @param mixed $dateDebut
+     */
+    public function setDateDebut($dateDebut)
+    {
+        $this->dateDebut = $dateDebut;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateFin()
+    {
+        return $this->dateFin;
+    }
+
+    /**
+     * @param mixed $dateFin
+     */
+    public function setDateFin($dateFin)
+    {
+        $this->dateFin = $dateFin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFrequence()
+    {
+        return $this->frequence;
+    }
+
+    /**
+     * @param mixed $frequence
+     */
+    public function setFrequence($frequence)
+    {
+        $this->frequence = $frequence;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValeur()
+    {
+        return $this->valeur;
+    }
+
+    /**
+     * @param mixed $valeur
+     */
+    public function setValeur($valeur)
+    {
+        $this->valeur = $valeur;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTypeDonneeMesuree()
+    {
+        return $this->typeDonneeMesuree;
+    }
+
+    /**
+     * @param mixed $typeDonneeMesuree
+     */
+    public function setTypeDonneeMesuree($typeDonneeMesuree)
+    {
+        $this->typeDonneeMesuree = $typeDonneeMesuree;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTypeCalcul()
+    {
+        return $this->typeCalcul;
+    }
+
+    /**
+     * @param mixed $typeCalcul
+     */
+    public function setTypeCalcul($typeCalcul)
+    {
+        $this->typeCalcul = $typeCalcul;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrevue()
+    {
+        return $this->prevue;
+    }
+
+    /**
+     * @param mixed $prevue
+     */
+    public function setPrevue($prevue)
+    {
+        $this->prevue = $prevue;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCheminFichierXml()
+    {
+        return $this->cheminFichierXml;
+    }
+
+    /**
+     * @param mixed $cheminFichierXml
+     */
+    public function setCheminFichierXml($cheminFichierXml)
+    {
+        $this->cheminFichierXml = $cheminFichierXml;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEtiquette()
+    {
+        return $this->etiquette;
+    }
+
+    /**
+     * @param mixed $etiquette
+     */
+    public function setEtiquette($etiquette)
+    {
+        $this->etiquette = $etiquette;
+    }
 }
--- service-web-lecture/api/modele/Mesure.php
+++ service-web-lecture/api/modele/Mesure.php
@@ -3,5 +3,92 @@
 
 class Mesure
 {
+    const CLE_ID_MESURE = "id_mesure";
+    const CLE_ID_HISTORIQUE_DONNEE_MESUREE = "id_historique_donnee_bouee";
+    const CLE_ID_TYPE_DONNEE_MESUREE = "id_type_donnee_mesuree";
+    const CLE_VALEUR = "valeur";
 
+    private $idMesure;
+    private $historiqueDonneeMesuree;
+    private $typeDonneeMesuree;
+    private $valeur;
+
+    /**
+     * Mesure constructor.
+     * @param $idMesure
+     * @param $historiqueDonneeMesuree
+     * @param $typeDonneeMesuree
+     * @param $valeur
+     */
+    public function __construct($idMesure, $historiqueDonneeMesuree, $typeDonneeMesuree, $valeur)
+    {
+        $this->idMesure = $idMesure;
+        $this->historiqueDonneeMesuree = $historiqueDonneeMesuree;
+        $this->typeDonneeMesuree = $typeDonneeMesuree;
+        $this->valeur = $valeur;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdMesure()
+    {
+        return $this->idMesure;
+    }
+
+    /**
+     * @param mixed $idMesure
+     */
+    public function setIdMesure($idMesure)
+    {
+        $this->idMesure = $idMesure;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHistoriqueDonneeMesuree()
+    {
+        return $this->historiqueDonneeMesuree;
+    }
+
+    /**
+     * @param mixed $historiqueDonneeMesuree
+     */
+    public function setHistoriqueDonneeMesuree($historiqueDonneeMesuree)
+    {
+        $this->historiqueDonneeMesuree = $historiqueDonneeMesuree;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTypeDonneeMesuree()
+    {
+        return $this->typeDonneeMesuree;
+    }
+
+    /**
+     * @param mixed $typeDonneeMesuree
+     */
+    public function setTypeDonneeMesuree($typeDonneeMesuree)
+    {
+        $this->typeDonneeMesuree = $typeDonneeMesuree;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValeur()
+    {
+        return $this->valeur;
+    }
+
+    /**
+     * @param mixed $valeur
+     */
+    public function setValeur($valeur)
+    {
+        $this->valeur = $valeur;
+    }
 }
