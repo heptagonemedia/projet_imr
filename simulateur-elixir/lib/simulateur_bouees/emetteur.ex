33db59b5450f07edef4f82313b548bad70170819
Unknown <lachance.christophe@cgmatane.qc.ca>
2019-10-23 08:54:15 -0400

    Emetteur socket
    Début de la définition des fonctions

--- simulateur-elixir/lib/simulateur_bouees/emetteur.ex
+++ simulateur-elixir/lib/simulateur_bouees/emetteur.ex
@@ -1,11 +1,33 @@
 defmodule SimulateurBouees.Emetteur do
-    use Agent
+    use GenServer
     
     def start_link(concentrateur) do
-      Task.start_link(fn -> loop(concentrateur) end)
+      GenServer.start_link(__MODULE__, concentrateur)
     end
   
-    defp loop(concentrateur) do
-      # Send 
+    def init(state) do
+      opts = [:binary, active: false]
+      {:ok, socket} = :gen_tcp.connect('localhost', 6379, opts)
+      {:ok, %{state | socket: socket}}
+    end
+
+    def command(pid, cmd) do
+      GenServer.call(pid, {:command, cmd})
+    end
+  
+    def handle_call({:command, cmd}, from, %{socket: socket} = state) do
+      :ok = :gen_tcp.send(socket, encode(cmd))
+  
+      # `0` means receive all available bytes on the socket.
+      {:ok, msg} = :gen_tcp.recv(socket, 0)
+      {:reply, decode(msg), state}
+    end
+
+    def encode(msg) do
+      msg
+    end
+
+    def decode(msg) do
+      msg
     end
   end
