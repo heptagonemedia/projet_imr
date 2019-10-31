324bb526b7962a0fb4aeb13e12920a3a79def716
loicbtd <loic.bert.marcel@gmail.com>
2019-10-31 19:50:22 -0400

    service web lecture : création du début du modèle

--- service-web-lecture/api/modele/Mesure.php
+++ service-web-lecture/api/modele/Mesure.php
@@ -3,5 +3,7 @@
 
 class Mesure
 {
-
+    const CLE_ID_MESURE = "id_mesure";
+    const CLE_RESULTAT = "resultat";
+    const CLE_VALEUR = "";
 }
---
+++ service-web-lecture/api/modele/Resultat.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class Resultat
+{
+    const CLE_ID_BOUEE = "id_";
+
+
+}
