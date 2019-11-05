0d6e16dc812f3ba5e8e8c95f224112586b597330
EstevanGAY <estevan.gay@gmail.com>
2019-11-05 16:06:17 -0500

    Gestion d'erreurs en javascript (côté client) effectuée

--- generation-donnees/index.php
+++ generation-donnees/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 
     <meta charset="UTF-8">
@@ -18,37 +19,36 @@
 
 <body>
 
-    <?php 
+    <?php
 
-        // echo '<pre>';
-        // print_r($_POST);
-        // echo '</pre>';
+    // echo '<pre>';
+    // print_r($_POST);
+    // echo '</pre>';
 
-        $connection = null;
-        $c = null;
+    // $connection = null;
+    // $c = null;
 
-        if (!empty($_POST) /*&& empty($erreur)*/) {
+    // if (!empty($_POST) /*&& empty($erreur)*/) {
 
-            $connection = new BaseDeDonnees();
-            //Connection à la BDD
-            $connection->connect();
+    //     $connection = new BaseDeDonnees();
+    //     //Connection à la BDD
+    //     $connection->connect();
 
-            //Récupération du lien
-            $c = $connection->getLien();
+    //     //Récupération du lien
+    //     $c = $connection->getLien();
 
-            if($connection) {
-                $connection->insert($_POST, "scenario", $c);
-            } else {
-                die("Erreur de connection");
-            }
+    //     if ($connection) {
+    //         $connection->insert($_POST, "scenario", $c);
+    //     } else {
+    //         die("Erreur de connection");
+    //     }
 
-            $connection->disconnect();
-
-        }
+    //     $connection->disconnect();
+    // }
 
     ?>
 
-    <div class="signupSection"> 
+    <div class="signupSection">
 
         <form action="#" method="POST" class="signupForm" name="signupform">
 
@@ -58,13 +58,12 @@
 
                 <div class="gauche">
                     <label for="type-scenario"></label>
-                    <input type="text" class="inputFields" id="type-scenario" name="type-scenario" placeholder="Type du scenario" 
-                    value="" required />
+                    <input type="text" class="inputFields" id="type-scenario" name="type-scenario" placeholder="Type du scenario" value="" oninput="testerSiNombre(this.value, this.id)" required />
                 </div>
 
                 <div class="droite">
                     <label for="prendre-compte"></label>
-                    <select name="prendre-compte" id="prendre-compte" class="inputFields" onsubmit="priseEnCompteValidation(valeur)">
+                    <select name="prendre-compte" id="prendre-compte" class="inputFields">
                         <option value="true">Oui</option>
                         <option value="false">Non</option>
                     </select>
@@ -76,109 +75,97 @@
 
                 <div class="gauche">
                     <label for="temperature"></label>
-                    <input type="text" class="inputFields" id="temperature" name="temperature" placeholder="Temperature valeur par défaut" 
-                    value="" required />
+                    <input type="text" class="inputFields" id="temperature" name="temperature" placeholder="Temperature valeur par défaut" value="" oninput="testerSiNombre(this.value, this.id)" required />
                 </div>
 
                 <div class="droite">
                     <label for="erreur-temp"></label>
-                    <input type="text" class="inputFields" id="erreur-temp" name="erreur-temp" placeholder="Temperature erreur" 
-                        value="" oninput="return erreurValidation(this.value, this.id)" required />
+                    <input type="text" class="inputFields" id="erreur-temp" name="erreur-temp" placeholder="Temperature erreur" value="" oninput="erreurValidation(this.value, this.id)" required />
                 </div>
 
             </div>
 
             <div class="conteneur-input">
-            
+
                 <div class="gauche">
                     <label for="salinite"></label>
-                    <input type="text" class="inputFields" id="salinite" name="salinite" placeholder="Salinité valeur par défaut" 
-                    value="" required />
+                    <input type="text" class="inputFields" id="salinite" name="salinite" placeholder="Salinité valeur par défaut" value="" oninput="testerSiNombre(this.value, this.id)" required />
                 </div>
-            
+
                 <div class="droite">
                     <label for="erreur-salinite"></label>
-                    <input type="text" class="inputFields" id="erreur-salinite" name="erreur-salinite" placeholder="Salinité erreur"
-                        value="" oninput="return erreurValidation(this.value, this.id)" required />
+                    <input type="text" class="inputFields" id="erreur-salinite" name="erreur-salinite" placeholder="Salinité erreur" value="" oninput="erreurValidation(this.value, this.id)" required />
                 </div>
-            
+
             </div>
 
             <div class="conteneur-input">
-            
+
                 <div class="gauche">
                     <label for="debit"></label>
-                    <input type="text" class="inputFields" id="debit" name="debit" placeholder="Débit valeur par défaut"
-                        value="" required />
+                    <input type="text" class="inputFields" id="debit" name="debit" placeholder="Débit valeur par défaut" value="" oninput="testerSiNombre(this.value, this.id)" required />
                 </div>
-            
+
                 <div class="droite">
                     <label for="erreur-debit"></label>
-                    <input type="text" class="inputFields" id="erreur-debit" name="erreur-debit" placeholder="Débit erreur"
-                        value="" oninput="return erreurValidation(this.value, this.id)" required />
+                    <input type="text" class="inputFields" id="erreur-debit" name="erreur-debit" placeholder="Débit erreur" value="" oninput="erreurValidation(this.value, this.id)" required />
                 </div>
-            
+
             </div>
 
             <div class="conteneur-input">
-            
+
                 <div class="gauche">
                     <label for="longitude"></label>
-                    <input type="text" class="inputFields" id="longitude" name="longitude" placeholder="Longitude valeur par défaut"
-                        value="" required />
+                    <input type="text" class="inputFields" id="longitude" name="longitude" placeholder="Longitude valeur par défaut" value="" oninput="testerSiNombre(this.value, this.id)" required />
                 </div>
-            
+
                 <div class="droite">
                     <label for="erreur-longitude"></label>
-                    <input type="text" class="inputFields" id="erreur-longitude" name="erreur-longitude" placeholder="Longitude erreur"
-                        value="" oninput="return erreurValidation(this.value, this.id)" required />
+                    <input type="text" class="inputFields" id="erreur-longitude" name="erreur-longitude" placeholder="Longitude erreur" value="" oninput="erreurValidation(this.value, this.id)" required />
                 </div>
-            
+
             </div>
 
             <div class="conteneur-input">
-            
+
                 <div class="gauche">
                     <label for="latitude"></label>
-                    <input type="text" class="inputFields" id="latitude" name="latitude" placeholder="Latitude valeur par défaut"
-                        value="" required />
+                    <input type="text" class="inputFields" id="latitude" name="latitude" placeholder="Latitude valeur par défaut" value="" oninput="testerSiNombre(this.value, this.id)" required />
                 </div>
-            
+
                 <div class="droite">
                     <label for="erreur-latitude"></label>
-                    <input type="text" class="inputFields" id="erreur-latitude" name="erreur-latitude" placeholder="Latitude erreur"
-                        value="" oninput="return erreurValidation(this.value, this.id)" required />
+                    <input type="text" class="inputFields" id="erreur-latitude" name="erreur-latitude" placeholder="Latitude erreur" value="" oninput="erreurValidation(this.value, this.id)" required />
                 </div>
-            
+
             </div>
 
             <div class="conteneur-input">
-            
+
                 <div class="gauche">
                     <label for="batterie"></label>
-                    <input type="text" class="inputFields" id="batterie" name="batterie" placeholder="Batterie valeur par défaut"
-                        value="" required />
+                    <input type="text" class="inputFields" id="batterie" name="batterie" placeholder="Batterie valeur par défaut" value="" oninput="erreurValeurBatterie(this.value); testerSiNombre(this.value, this.id);" required />
                 </div>
-            
+
                 <div class="droite">
                     <label for="decr-batterie"></label>
-                    <input type="text" class="inputFields" id="decr-batterie" name="decr-batterie" placeholder="Batterie décrémentation"
-                        value="" oninput="" required />
+                    <input type="text" class="inputFields" id="decr-batterie" name="decr-batterie" placeholder="Batterie décrémentation" value="" oninput="erreurEntreeDecrementationBatterie(this.value)" required />
                 </div>
-            
+
             </div>
 
             <div class="conteneur-input">
                 <label for="description"></label>
-                <textarea name="description" rows="5" id="description" class="inputFields" placeholder="Description du scénario" value=""></textarea>
+                <textarea name="description" rows="5" id="description" class="inputFields" placeholder="Description du scénario" value="" oninput="estDescriptionRemplie(this.value)" required></textarea>
             </div>
-            
+
 
             <div>
                 <input type="submit" id="ajouter-btn" name="ajouter" alt="ajouter" value="Ajouter">
             </div>
 
-        </form> 
+        </form>
 
 
     </div>
--- generation-donnees/js/index.js
+++ generation-donnees/js/index.js
@@ -1,40 +1,99 @@
 var alertRedInput = "#8C1010";
 var defaultInput = "rgba(10, 180, 180, 1)";
 
-function priseEnCompteValidation(valeur) {
 
-    var prendreEnCompte = document.getElementById("prendre-compte");
-    var erreur = [];
-
-    if (valeur === "compte"){
-        erreur.push("Vous devez choisir une valeur");
-    }
+function testerErreur(element, erreur) {
 
     if (erreur.length > 0) {
-        prendreEnCompte.setCustomValidity(erreur.join("\n"));
-        prendreEnCompte.style.borderColor = alertRedInput;
+
+        element.setCustomValidity(erreur.join("\n"));
+        element.style.borderColor = alertRedInput;
+
     } else {
-        prendreEnCompte.setCustomValidity("");
-        prendreEnCompte.style.borderColor = defaultInput;
+
+        element.setCustomValidity("");
+        element.style.borderColor = defaultInput;
+
     }
 
 }
+
+
+function testerSiNombre(valeur, id) {
+
+    var element = document.getElementById(id);
+    var erreur = [];
+
+    if (((!(/^\d+$/.test(valeur)) && !(/^\d+\.\d+$/.test(valeur))) && (!(/^\-\d+$/).test(valeur) && !(/^\-\d+\.\d+$/).test(valeur)))) {
+        erreur.push("La valeur doit être un nombre entier");
+    }
+
+    testerErreur(element, erreur);
+
+}
+
 
 function erreurValidation(valeur, id) {
 
     var elementErreur = document.getElementById(id);
     var erreur = [];
 
-    if (valeur<-1 || valeur>1) {
+    if ((!(/^\d+$/.test(valeur)) && !(/^\d+\.\d+$/.test(valeur))) && (!(/^\-\d+$/).test(valeur) && !(/^\-\d+\.\d+$/).test(valeur))) {
+        erreur.push("La valeur doit être un nombre entier");
+    }
+
+    if (valeur < -1 || valeur > 1) {
         erreur.push("La valeur doit être comprise entre -1 et 1 incluent");
     }
 
-    if (erreur.length > 0) {
-        elementErreur.setCustomValidity(erreur.join("\n"));
-        elementErreur.style.borderColor = alertRedInput;
-    } else {
-        elementErreur.setCustomValidity("");
-        elementErreur.style.borderColor = defaultInput;
+    testerErreur(elementErreur, erreur);
+
+}
+
+
+function erreurEntreeDecrementationBatterie(valeur) {
+    
+    var element = document.getElementById("decr-batterie");
+    var erreur = [];
+
+    if ( !(/^\d+$/.test(valeur)) && !(/^\d+\.\d+$/.test(valeur)) ) {
+        erreur.push("La valeur doit être un nombre entier");
     }
+
+    if (valeur < 0) {
+        erreur.push("La valeur doit être positive ou nulle");
+    }
+
+    testerErreur(element, erreur);
+
+    
+
+}
+
+
+function erreurValeurBatterie(valeur) {
+
+    var elementBatterie = document.getElementById("batterie");
+    var erreur = [];
+
+    if (valeur <= 0) {
+        erreur.push("La valeur doit être strictement positive");
+    }
+
+    testerErreur(elementBatterie, erreur);
+
+}
+
+
+function estDescriptionRemplie(valeur) {
+
+    var elementDescription = document.getElementById("batterie");
+    var erreur = [];
+
+    if (valeur === null || valeur === undefined) {
+        erreur.push("Vous devez entrer une description");
+    }
+
+    testerErreur(elementDescription, erreur);
 
 }
