70af23d6e1f4ffd2c598b32272e71e77b484e75c
Unknown <lachance.christophe@cgmatane.qc.ca>
2019-11-05 15:16:15 -0500

    Bouee.init
    Valeurs initiales placeholdee

--- simulateur-elixir/lib/simulateur_bouees/bouee.ex
+++ simulateur-elixir/lib/simulateur_bouees/bouee.ex
@@ -1,9 +1,24 @@
 defmodule SimulateurBouees.Bouee do
   use Agent
 
-  def start_link(state) do
+  def start_link(initial) do
+    state = init(initial)
     Agent.start_link(process(state))
   end
+
+def init(initial) do
+  state = %{id_bouee: initial.idbouee, scenario: initial.scenario}
+  temperature = Enum.random(5..10)
+  salinite = Enum.random(5..10)
+  debit = Enum.random(5..10)
+  valsInit = %{temperature: temperature, salinite: salinite, debit: debit}
+  latitude = :rand.uniform(20)
+  longitude = :rand.uniform(20)
+  batterie = 100
+  valsInit = %{valsInit | longitude: longitude, latitude: latitude}
+  state = %{state | valeurs_initiales: valsInit}
+  state
+end
 
   def process(state) do
     receive do
@@ -15,6 +30,7 @@ defmodule SimulateurBouees.Bouee do
     end
 
     def generer(state) do
-      
+
+      SimulateurBouees.Concentrateur.put()
     end
 end
