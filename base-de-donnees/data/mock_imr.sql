f3473d4619e899f0ff7e46428ae03b807f75c1ac
loicbtd <loic.bert.marcel@gmail.com>
2019-11-13 10:25:42 -0500

    correction des ajouts de données prédéfinies

--- base-de-donnees/data/mock_imr.sql
+++ base-de-donnees/data/mock_imr.sql
@@ -1,8 +1,8 @@
-\COPY region (etiquette) FROM './mockdata/region.csv' DELIMITER ',' CSV;
+\COPY region (id_region,etiquette) FROM './mockdata/region.csv' DELIMITER ',' CSV;
 \COPY bouee (etiquette, longitude_reference, latitude_reference, id_region) FROM './mockdata/bouee.csv' DELIMITER ',' CSV;
-\COPY type_donnee (etiquette, unite) FROM './mockdata/type_donnee_mesuree.csv' DELIMITER ',' CSV;
+\COPY type_donnee (id_type_donnee,etiquette, unite) FROM './mockdata/type_donnee_mesuree.csv' DELIMITER ',' CSV;
 \COPY historique_donnee_bouee (id_bouee, date_saisie) FROM './mockdata/historique_donnee_bouee.csv' DELIMITER ',' CSV;
 \COPY mesure (id_historique_donnee_bouee, id_type_donnee, valeur) FROM './mockdata/mesure.csv' DELIMITER ',' CSV;
-\COPY type_calcul (etiquette) FROM './mockdata/type_calcul.csv' DELIMITER ',' CSV;
+\COPY type_calcul (id_type_calcul,etiquette) FROM './mockdata/type_calcul.csv' DELIMITER ',' CSV;
 \COPY calcul (etiquette, date_generation, date_prochaine_generation, date_debut_plage, date_fin_plage, frequence_valeur, enregistre, id_bouee, id_type_calcul) FROM './mockdata/calcul.csv' DELIMITER ',' CSV;
 \COPY resultat (id_type_donnee, id_calcul, xml_graphique) FROM './mockdata/resultat.csv' DELIMITER ',' CSV;
