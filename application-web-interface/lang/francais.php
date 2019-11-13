fc633c9e0869ec06ad615ff3ff4d51cec8356c3e
BigLazyBrother <tcousson@wanadoo.fr>
2019-11-13 10:02:24 -0500

    Début du système de traduction.

---
+++ application-web-interface/lang/francais.php
@@ -0,0 +1,68 @@
+<?php
+// L'idée de l'algorithme a été détaillée dans la documentation,
+// dans la branche accessibilite-master dans administratif/Accessibilite/doc-accessibilite.md
+
+$francais = array(
+	// accueil.php
+	"titleAccueil"=>"AccueilIMR",
+	"texteBoueeFonctionnelle"=>"Fonctionnelle",
+	"texteBoueeProbleme"=>"Problème",
+	"derniereMiseAjour"=>"Dernière mise &agrave; jour : DATE et HEURE",
+	"titreCarte"=>"Carte des bouées",
+	"boutonPleinEcran"=>"Plein &eacute;cran",
+	"boueesNonConformes"=>"Bou&eacute;es non conforme",
+	// footer.php
+	"copyright"=>"&copy; 2019 Heptagone Media",
+	// formulaireRecherche.php
+	"titleFormulaire"=>"Formulaire",
+	"titreCalcul"=>"Calcul",
+	"moyenne"=>"Moyenne",
+	"mediane"=>"M&eacute;diane",
+	"ecartType"=>"&Eacute;cart type",
+	"frequence"=>"Fr&eacute;quence",
+	"annee"=>"Année",
+	"mois"=>"Mois",
+	"jour"=>"Jour",
+	"heure"=>"Heure",
+	"minute"=>"Minute",
+	"helperValeur"=>"Il faut saisir au moins une valeur et uniquement des entiers",
+	"bouee"=>"Bouée",
+	"numero"=>"Numero",
+	"helperNumero"=>"Il faut entrer un entier compris entre 1 et 75",
+	"intervalle"=>"Intervalle temporel",
+	"dateDebut"=>"Date début",
+	"heureDebut"=>"Heure début",
+	"dateFin"=>"Date fin",
+	"heureFin"=>"Heure fin",
+	"helperDate"=>"Il faut que la date de fin soit supérieure à celle de début",
+	"enregistrerCalcul"=>"Enregistrer calcul",
+	"valider"=>"Valider",
+	// header
+	"menu"=>"menu",
+	"accueilMenu"=>"accueil",
+	"formulaire"=>"formulaire",
+	"resultatCalcul"=>"Résultat du calcul",
+	"calculDeuxPoints"=>"Calcul : ",
+	// navigation_side_bar
+	"nomIMR"=>"Institut Maritime de Rimouski",
+	"AccueilNavigation"=>"Accueil",
+	"accesCalcul"=>"Accéder aux calculs",
+	"calculEnregistres"=>"Calculs Enregistrés",
+	"unTelCalcul"=>"Calcul",
+	"calculEnCours"=>"Calcul en cours",
+	"faireDesCalculs"=>"Faire des calculs",
+	"nouveauCalcul"=>"Nouveau calcul",
+	// resultats.php
+	"titleResultat"=>"Resultats",
+	"vitesseCourant"=>"Vitesse du courant",
+	"agrandirCarte"=>"Agrandir la Carte",
+	"consulterDonneesBrutes"=>"Consulter les données brutes",
+	"salinite"=>"Salinité",
+	"temperature"=>"Température",
+	"calculEnregistre"=>"Calcul enregistré",
+	"calculBienEnregistre"=>"le calcul a bien été enregitré",
+	"ok"=>"ok"
+);
+
+
+?>
