db7543eb295fe30016945d7ca8ad655b465e0978
Louis Pascuzzi <louis.pascuzzi@edu.univ-fcomte.fr>
2019-11-27 16:16:46 -0500

    ajout du DAO regionDAO

--- application-web-laravel/app/Data/RegionDAO.php
+++ application-web-laravel/app/Data/RegionDAO.php
@@ -1,8 +1,39 @@
 <?php
 
 namespace App\Data;
+use App\Models\Region;
+use Illuminate\Support\Facades\DB;
 
 class RegionDAO implements RegionSQL
 {
+    private $instance;
+    private $connection;
 
+    private $listeRegions;
+
+    public static function getInstance()
+    {
+        if(is_null(self::$instance)) {
+            self::$instance = new ResultatDAO();
+        }
+        return self::$instance;
+    }
+
+    public function __construct()
+    {
+        $listeRegions = array();
+    }
+
+    public function recuperListeRegions(){
+        $listeRegions = array();
+
+        $regions = DB::select('select * from region');
+
+        foreach ($regions as $region){
+            array_push($listeRegions, new Region($region["id"], $regions["etiquette"]));
+        }
+
+        return $listeRegions;
+    }
+    
 }
