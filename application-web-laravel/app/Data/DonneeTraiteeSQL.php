d90cbc777f07934893809d1c162934e25a45de2a
Louis Pascuzzi <louis.pascuzzi@edu.univ-fcomte.fr>
2019-11-27 09:35:28 -0500

    suppression d'interfaces et de DAO inutiles

--- application-web-laravel/app/Data/DonneeTraiteeSQL.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Data;
-
-interface DonneeTraiteeSQL
-{
-    const SQL_LISTER_DONNEE_TRAITEE = "";
-}
--- application-web-laravel/app/Data/TypeDonneeMesureeDAO.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Data;
-
-class TypeDonneeMesureeDAO implements TypeDonneeMesureeSQL
-{
-
-}
